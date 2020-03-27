42d484e1384da884cc4be7cbeb4df558e6ec77a4
abbadon1334 <fdanti@gmail.com>
2020-03-27 10:19:27 +0100

    wip

--- demo/simple/index.php
+++ demo/simple/index.php
@@ -30,26 +30,15 @@ class ApplicationUser extends Model
 $db = new atk4\data\Persistence\SQL('sqlite::memory:');
 Migration::getMigration(new ApplicationUser($db))->migrate();
 
-$app = new App([
-    'title' => 'Auth0 test',
-    'always_run' => false,
-    'call_exit' => false
-]);
+$app = new App(['title' => 'Auth0 test']);
 
 $app->initLayout(Centered::class);
-
-$app->addHook('onBeforeUserLogin', function(App $app) {
-});
-
-$app->addHook('onAfterUserLogin', function(App $app, ApplicationUser $user) {
-    $app->redirect('/'); // clear url
-});
-
-$app->addHook('onBeforeUserLogout', function(App $app, ApplicationUser $user) {
-});
-
-$app->addHook('onAfterUserLogout', function(App $app) {
-});
+/*
+$app->addHook('onBeforeUserLogin', function(App $app) {});
+$app->addHook('onAfterUserLogin', function(App $app, ApplicationUser $user) {});
+$app->addHook('onBeforeUserLogout', function(App $app, ApplicationUser $user) {});
+$app->addHook('onAfterUserLogout', function(App $app) {});
+*/
 
 $app->add([
     Auth0::class,
@@ -58,8 +47,8 @@ $app->add([
             'domain' => AUTH0_DOMAIN,
             'client_id' => AUTH0_CLIENT_ID,
             'client_secret' => AUTH0_CLIENT_SECRET,
-            'redirect_uri' => 'http://127.0.0.1/index.php?atk_centered_auth0_callback=callback&__atk_callback=1',
-            'returnTo' => 'http://127.0.0.1/',
+            'redirect_uri' => 'https://local.auth0/index.php?atk_centered_auth0_callback=callback&__atk_callback=1',
+            'returnTo' => 'https://local.auth0/',
             'scope' => 'profile email',
             'persist_id_token' => true,
             //'persist_access_token' => true,
